3c847a04fad5fb6efda6eb8efc3428a4b7a5815f
alsqja <alsqja2626@uno-solution.com>
2023-01-11 15:56:54 +0900

    크기가 작은 부분 문자열 python

---
+++ python 문법 연습/크기가 작은 부분 문자열.py
@@ -0,0 +1,14 @@
+def solution(t, p):
+  answer = 0
+  start = 0
+  end = len(p)
+
+  while (end <= len(t)):
+    if (int(t[start:end]) <= int(p)):
+      answer += 1
+    start += 1
+    end += 1
+  
+  return answer
+
+print(solution("500220839878", '7'))
