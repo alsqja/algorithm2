ed741a48a6d36af63c035a8fc7820f85bc6c495d
alsqja <alsqha2626@gmail.com>
2023-02-01 15:24:49 +0900

    뒤에 있는 큰 수 찾기 python

--- python 문법 연습/뒤에 있는 큰 수 찾기.py
+++ python 문법 연습/뒤에 있는 큰 수 찾기.py
@@ -17,3 +17,17 @@ def solution(numbers):
 
 
 print(solution([2, 3, 3, 5]))
+
+
+def solution(numbers):
+    answer = [-1] * len(numbers)
+    stack = []
+
+    for i in range(len(numbers)):
+        # 스택에 인덱스가 존재하고, 마지막 인덱스의 숫자가 i번째 숫자보다 작다면
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]  # 스택에서 제외시키고, 해당 인덱스 값을 바꿔주기
+
+        stack.append(i)  # 스택에 인덱스 넣기
+
+    return answer
