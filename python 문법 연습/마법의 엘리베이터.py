23ae0719bac6e49858d191cc6b915208cb51192f
alsqja <alsqha2626@gmail.com>
2023-02-07 15:35:16 +0900

    마법의 엘리베이터 python

--- python 문법 연습/마법의 엘리베이터.py
+++ python 문법 연습/마법의 엘리베이터.py
@@ -1,4 +1,22 @@
 def solution(storey):
     answer = 0
 
+    while storey > 0:
+        num = storey % 10
+        storey = storey // 10
+        if num > 5:
+            answer += 10 - num
+            storey += 1
+        elif num == 5:
+            if storey % 10 > 4:
+                answer += 10 - num
+                storey += 1
+            else:
+                answer += num
+        else:
+            answer += num
+
     return answer
+
+
+print(solution(97))
