0fa51fd0b890f4c4f395a574355f421b4790fc91
alsqja <alsqha2626@gmail.com>
2023-02-17 14:19:59 +0900

    add 다음에 올 숫자.py

---
+++ python 문법 연습/다음에 올 숫자.py
@@ -0,0 +1,4 @@
+def solution(common):
+    if common[-1] - common[-2] == common[-2] - common[-3]:
+        return common[-1] + common[-1] - common[-2]
+    return common[-1] * common[-1] / common[-2]
